83cdf8907df58321fcd1b17d3f043bd559a26543
lineasp <82105227+lineasp@users.noreply.github.com>
2022-09-22 14:35:40 +0200

    ---

---
+++ CoreAdminLTE/Pages/Employee.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreAdminLTE.Pages
+{
+    public class EmployeeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
